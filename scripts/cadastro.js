370138077ae47885fb655a4ec5867f039f1e0788
Matheus Keher <113534686+matheusjbk@users.noreply.github.com>
2022-11-06 17:04:21 -0300

    Add files via upload

---
+++ scripts/cadastro.js
@@ -0,0 +1,107 @@
+//Declaração de variáveis
+let nome=document.querySelector('#name');
+let sobrenome=document.querySelector('#lastName');
+let rg=document.querySelector('#rg');
+let email=document.querySelector('#email');
+let senha=document.querySelector('#senha');
+let senha2=document.querySelector('#senha2');
+let cep=document.querySelector('#cep');
+let numero=document.querySelector('#numero');
+let btn=document.querySelector('#btnCadastro');
+let error=document.querySelector('#error');
+
+//Função para validação de senha
+const validaSenha=()=>{
+    if(senha.length<8){
+        return 'err';
+    }else{
+        return 'success';
+    }
+}
+
+//Função para comparar se as senhas estão iguais
+const senhaIgual=()=>{
+    if(senha2==''){
+        return 'err';
+    }else if(senha2!=senha.value){
+        return 'err';
+    }else{
+        return 'success';
+    }
+}
+
+//Função para limpar o formulário em caso de preenchimento errado
+const limparForm=()=>{
+    document.querySelector('#uf').value='';
+    document.querySelector('#localidade').value='';
+    document.querySelector('#bairro').value='';
+    document.querySelector('#logradouro').value='';
+}
+
+//Função para fazer validação do CEP
+const cepValido=(cep)=>cep.value.length==9;
+
+//Função para preencher o formulário assim que confirmado o CEP
+const preencherForm=(result)=>{
+    for(const campo in result){
+        if(document.querySelector('#'+campo)){
+            document.querySelector('#'+campo).value=result[campo];
+        }
+    }
+}
+
+//Função para encontrar os dados do CEP na API
+cep.addEventListener('focusout', async()=>{
+    limparForm();
+
+    const search=cep.value.replace('-', '');
+
+    if(cepValido(cep)){
+        const response=await fetch(`http://viacep.com.br/ws/${search}/json/`);
+        const dados=await response.json();
+        if(dados.hasOwnProperty('erro')){
+            alert('Erro! CEP não encontrado.');
+        }else{
+            preencherForm(dados);
+        }
+    }else{
+        alert('CEP digitado incorretamente!');
+    }
+
+});
+
+//Função para fazer validação das funções
+btn.addEventListener('click', (e)=>{
+    e.preventDefault();
+    let nomeValue=nome.value;
+    let sobrenomeValue=sobrenome.value;
+    let rgValue=rg.value;
+    let emailValue=email.value;
+    let senhaValue=senha.value;
+    let senha2Value=senha2.value;
+    let numeroValue=numero.value;
+
+    if(nomeValue=='' || sobrenomeValue=='' || rgValue=='' || emailValue=='' || senhaValue=='' || senha2Value=='' || numeroValue==''){
+        error.innerHTML='Por favor, preencha todos os campos!';
+    }else{
+        alert('Cadastro feito com sucesso!');
+        nomeValue='';
+        sobrenomeValue='';
+        rgValue='';
+        emailValue='';
+        senhaValue='';
+        senha2Value='';
+        numeroValue='';
+        document.querySelector('#cep').value='';
+        document.querySelector('#uf').value='';
+        document.querySelector('#localidade').value='';
+        document.querySelector('#bairro').value='';
+        document.querySelector('#logradouro').value='';
+        document.querySelector('#complemento').value='';
+    }
+});
+
+//Máscaras para dados
+$("#rg").mask("99.999.999-9");
+
+$('#cep').mask('99999-999');
