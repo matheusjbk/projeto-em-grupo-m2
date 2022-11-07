2ae8d227e3271f49398f9535d29e6f0ad3c60735
Matheus Keher <113534686+matheusjbk@users.noreply.github.com>
2022-11-07 08:23:54 -0300

    Update recuperacao.js

--- scripts/recuperacao.js
+++ scripts/recuperacao.js
@@ -5,9 +5,16 @@ btnRec.addEventListener('click', (event)=>{
     event.preventDefault();
     let emailValue=email.value;
     let padraoEmail=/^(\w|\.|-){3,20}@\w+\.(com(\.br)?|net)$/gi;
+    
+    if(padraoEmail.test(emailValue)){
+        alert('E-mail enviado, verifique sua caixa de entrada!');
+        window.location.href="./login.html";
+    }else{
+        alert('Digite um e-mail válido.');
+    }
 
-    const emailValido=padraoEmail.test(emailValue)?alert('E-mail enviado, verifique sua caixa de entrada!'): alert('Digite um e-mail válido.');
 
-    window.location.href="./login.html";
-    return emailValido;
+    // const emailValido=padraoEmail.test(emailValue)?alert('E-mail enviado, verifique sua caixa de entrada!'): alert('Digite um e-mail válido.');
+    // window.location.href="./login.html";
+    // return emailValido;
 });
